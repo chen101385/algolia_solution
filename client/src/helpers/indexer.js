26e408566db293f0e96fe066bb9ae1805a2d75c2
Chris <chen1385@gmail.com>
2018-06-18 19:34:38 -0700

    :arrow_up: installed algoliasearch & algoliasearch-helper packages, ran indexer helper to add records to restaurant index

---
+++ client/src/helpers/indexer.js
@@ -0,0 +1,14 @@
+const { mergedData } = require('./fileMerge'),
+    algoliasearch = require('algoliasearch')
+    KEY = require('../../../API/API_KEY'),
+    client = algoliasearch('TGMMPVICOC', KEY.API_KEY),
+    index = client.initIndex('restaurants');
+
+    index.addObjects(mergedData, (err, content) => {
+        if (err) console.log('error in adding objects :', err);
+        else console.log('objects were successfully added!');
+    });
+
+
+
+
